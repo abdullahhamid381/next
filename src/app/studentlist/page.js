966d4e1ca1e0cc9c164d9167fd310699ffe42e1e
abdullahhamid381 <hafizabdullahqurashi1@gmail.com>
2024-01-24 18:29:48 +0500

    dfdfggdf

--- src/app/studentlist/page.js
+++ src/app/studentlist/page.js
@@ -1,25 +1,33 @@
-'use client'
-// StudentList.js
-import Link from "next/link";
+import Link from "next/link"
 
-const studentData = [
-  { id: 1, name: "Abdullah" },
-  { id: 2, name: "Hamid" },
-  { id: 3, name: "John" },
-  { id: 4, name: "Emma" },
-  // Add more students as needed
-];
-
-export default function StudentList() {
-  return (
+const data=[
+  {
+    id:'1',
+    name:'Abdullah',
+  },
+  {
+    id:'2',
+    name:'Hanzla',
+  },
+  {
+    id:'3',
+    name:'Ahmad'
+  },
+]
+const studentdata=()=>{
+  return(
     <div>
-      <h1>Student List</h1>
-      {studentData.map((student) => (
-        <Link key={student.id} href={`/studentlist/${student.name}-${student.id}`}>
-          <h1>{student.name}</h1>
-        </Link>
-      ))}
-      <Link href='/'>home</Link>
+      {
+        data.map((item)=>{
+          return(
+            <div>
+              <Link key={item.id} href={`/studentlist/${item.name}-${item.id}`}>    <h1>{item.name}</h1></Link>
+          
+            </div>
+          )
+        })
+      }
     </div>
-  );
+  )
 }
+export default studentdata
--- src/app/studylist/[...study]/page.js
+++ src/app/studylist/[...study]/page.js
@@ -3,6 +3,7 @@ export default function({params}){
  <div>
            <h1>lecturre {params.study[0]}</h1>
            <p>Day : {params.study[1]}</p>
+           <p>Teacher : {params.study[2]}</p>
  </div>
     )
 }
